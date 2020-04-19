36d6e009b9bc77d87d5fafb9091d62fa1a8032e5
ing.conti <ingconti@ingconti.com>
2020-04-19 17:57:52 +0200

    Initial commit

---
+++ src/main/java/com/ingconti/App.java
@@ -0,0 +1,13 @@
+package com.ingconti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
