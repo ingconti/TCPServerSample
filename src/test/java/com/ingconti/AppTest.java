86d744cd628b798f8c0ae7c425555db977317428
ingconti <ingconti@ ingconti.com>
2024-02-28 18:56:23 +0100

    fixed for jdk 21

--- src/test/java/com/ingconti/AppTest.java
+++ src/test/java/com/ingconti/AppTest.java
@@ -1,19 +1,37 @@
 package com.ingconti;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
-
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
+    extends TestCase
 {
     /**
-     * Rigorous Test :-)
+     * Create the test case
+     *
+     * @param testName name of the test case
      */
-    @Test
-    public void shouldAnswerWithTrue()
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
     {
         assertTrue( true );
     }
